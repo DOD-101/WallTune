b6f37c23b87e2ccc88c9177cbc29a739a1f30582
David Thievon <pdkfan@gmail.com>
2024-02-12 17:35:53 +0100

    Can now fetch more than 50 tracks
    Added the ability for the spotify_api/main.py
    to fetch more than 50 tracks.

--- spotify_api/main.py
+++ spotify_api/main.py
@@ -9,6 +9,8 @@ import argparse
 from os import getcwd
 from os.path import join
 from urllib.request import urlretrieve
+from urllib.parse import quote
+from re import sub
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -19,18 +21,37 @@ load_dotenv()
 SCOPE = "user-library-read"
 
 
-def main(path: str, amount: int):
+def sanitize_filename(filename):
+    """
+    Makes sure all files have valid names to be saved.
+    """
+    safe_filename = quote(filename.encode("utf-8"), safe=" ")
+    # Replace any character that is not alphanumeric, dash, underscore, or space with an underscore
+    safe_filename = sub(r"[^a-zA-Z0-9\-\s]", "_", safe_filename)
+    return safe_filename
+
+
+def main(path: str, amount: int, offset: int):
     """Main function to get and save the images."""
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
 
-    results = sp.current_user_saved_tracks(limit=amount)
-    for idx, item in enumerate(results["items"]):
-        track = item["track"]
-        print(idx, track["artists"][0]["name"], " - ", track["name"])
-        urlretrieve(
-            track["album"]["images"][0]["url"],
-            join(path, f"{track['name']}_albumCover.jpg"),
-        )
+    total_fetched = 0
+    while total_fetched < amount:
+        limit = min(50, amount - total_fetched)
+        results = sp.current_user_saved_tracks(limit=limit, offset=offset)
+        total_fetched += len(results["items"])
+        offset += limit
+
+        for idx, item in enumerate(results["items"]):
+            track = item["track"]
+            safe_track_name = sanitize_filename(track["name"])
+            print(idx, track["artists"][0]["name"], " - ", track["name"])
+            urlretrieve(
+                track["album"]["images"][0]["url"],
+                join(path, f"{safe_track_name}_albumCover.jpg"),
+            )
+
+    print("Total fetched:", total_fetched)
 
 
 if __name__ == "__main__":
@@ -59,8 +80,17 @@ if __name__ == "__main__":
         help="The amount of images to get. Default: 20",
     )
 
+    parser.add_argument(
+        "-o",
+        "--offset",
+        metavar="",
+        type=int,
+        default=0,
+        help="The offset of your most recent liked songs to start from.",
+    )
+
     args = parser.parse_args()
 
     # endregion
 
-    main(args.path, args.amount)
+    main(args.path, args.amount, args.offset)
