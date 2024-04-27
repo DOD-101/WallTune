d09c311e891b6750ad8dac85b67aeef4be0e6984
David Thievon <pdkfan@gmail.com>
2024-04-27 12:01:43 +0200

    Added the ability to get songs from playlists as well

--- spotify_api/main.py
+++ spotify_api/main.py
@@ -7,7 +7,7 @@ TO-DO:
 
 import argparse
 from sys import exit as sysexit
-from os import getcwd, makedirs
+from os import makedirs
 from os.path import join
 from urllib.request import urlretrieve
 from urllib.parse import quote
@@ -23,7 +23,27 @@ load_dotenv()
 SCOPE = "user-library-read"
 
 
-def sanitize_filename(filename):
+def get_playlist_id(client: spotipy.Spotify, playlist_name: str):
+    """
+    Searches the user's playlists and returns the playlist ID of the playlist
+    with the name matching playlist_name.
+    If no matching playlist is found, exits.
+    """
+    offset = 0
+    total = 9999  # run the loop least once
+    while offset + 50 <= total:
+        playlists = client.current_user_playlists(limit=50, offset=offset)
+        total = playlists["total"]
+        for playlist in playlists["items"]:
+            if playlist["name"] == playlist_name:
+                return playlist["id"]
+
+        offset += 50
+    print(Fore.RED + "Error: " + Fore.RESET + "Couldn't find requested playlist.")
+    sysexit(1)
+
+
+def sanitize_filename(filename: str):
     """
     Makes sure all files have valid names to be saved.
     """
@@ -33,43 +53,55 @@ def sanitize_filename(filename):
     return safe_filename
 
 
-def main(save_path: str, amount: int, offset: int, create_no_dirs: bool):
+def main(
+    save_path: str,
+    amount: int,
+    offset: int,
+    create_no_dirs: bool,
+    playlist: str,
+):
     """Main function to get and save the images."""
-
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
 
     if not create_no_dirs:
         makedirs(save_path, exist_ok=True)
 
-    total_fetched = 0
-    while total_fetched < amount:
-        limit = min(50, amount - total_fetched)
-        results = sp.current_user_saved_tracks(limit=limit, offset=offset)
-        total_fetched += len(results["items"])
-        offset += limit
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
 
-        for idx, item in enumerate(results["items"]):
-            track = item["track"]
-            safe_track_name = sanitize_filename(track["name"])
-            print(idx, track["artists"][0]["name"], " - ", track["name"])
-            try:
-                urlretrieve(
-                    track["album"]["images"][0]["url"],
-                    join(save_path, f"{safe_track_name}_albumCover.jpg"),
+    if not playlist == "":
+        playlist_id = get_playlist_id(sp, playlist)
+        results = sp.playlist(playlist_id)["tracks"]["items"]
+    else:
+        results = []
+
+        total_fetched = 0
+        while total_fetched < amount:
+            limit = min(50, amount - total_fetched)
+            results += sp.current_user_saved_tracks(limit=limit, offset=offset)["items"]
+            total_fetched = len(results)
+            offset += limit
+
+    for idx, item in enumerate(results):
+        track = item["track"]
+        safe_track_name = sanitize_filename(track["name"])
+        print(idx, track["artists"][0]["name"], " - ", track["name"])
+        try:
+            urlretrieve(
+                track["album"]["images"][0]["url"],
+                join(save_path, f"{safe_track_name}_albumCover.jpg"),
+            )
+        except Exception as e:
+            if create_no_dirs and isinstance(e, FileNotFoundError):
+                print(
+                    Fore.RED
+                    + "Error: "
+                    + Fore.RESET
+                    + f"Directory {save_path} doesn't exist and -n / --create-no-dirs is set."
                 )
-            except Exception as e:
-                if create_no_dirs and isinstance(e, FileNotFoundError):
-                    print(
-                        Fore.RED
-                        + "Error: "
-                        + Fore.RESET
-                        + f"Directory {save_path} doesn't exist and -n / --create-no-dirs is set."
-                    )
-                    sysexit(1)
+                sysexit(1)
 
-                raise e
+            raise e
 
-    print("Total fetched:", total_fetched)
+    print("Total fetched:", len(results))
 
 
 if __name__ == "__main__":
@@ -85,8 +117,16 @@ if __name__ == "__main__":
         "path",
         metavar="path",
         type=str,
-        default=join(getcwd(), "imgs"),
         help="Where to save the files.",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--playlist",
+        metavar="",
+        type=str,
+        default="",
+        help="Instead of getting songs from the liked songs get it from the specified playlist.",
     )
 
     parser.add_argument(
@@ -95,7 +135,7 @@ if __name__ == "__main__":
         metavar="",
         type=int,
         default=20,
-        help="The amount of images to get. Default: 20",
+        help="The amount of images to get. Doesn't work with playlists. Default: 20",
     )
 
     parser.add_argument(
@@ -108,8 +148,8 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "--create-no-dirs",
         "-n",
+        "--create-no-dirs",
         action="store_true",
         help="The opposite of -d. Disallows the creation of any directories.",
     )
@@ -123,4 +163,5 @@ if __name__ == "__main__":
         args.amount,
         args.offset,
         args.create_no_dirs,
+        args.playlist,
     )
