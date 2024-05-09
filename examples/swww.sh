a6e0b17bc8f6df71fb8b8f2c006d4316f210ab8f
David Thievon <pdkfan@gmail.com>
2024-05-09 14:26:16 +0200

    Updated the example to now work with the currently playing song

--- examples/swww.sh
+++ examples/swww.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+
+
+# -- variables--
+
 # Directory containing the images
 # also counts all subdirectories
 directory="/path/to/images/"
-# where the scripts from github are 
-scripts="./WallTune-Usage/"
+# where the scripts from github are
+scripts="/path/to/git/repo/"
+# where songs from currently playing items are stored
+current_dir="/path/to/save/currently/playing/images/"
+# time before background is changed
+sleep_time_default=60
 
-# --Code-- 
+# --Code--
 
 # For Logging
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+sleep_time="$sleep_time_default"
 
 exec > "$SCRIPT_DIR/swww.sh.log"
 exec 2>&1
@@ -26,11 +36,18 @@ IFS=$'\n' read -rd '' -a files <<<"$all_files"
 
 get_file() {
 
-    # Generate a random index within the range of available file paths
-    index=$((RANDOM % ${#files[@]}))
+    current_img=$(python "$scripts/spotify_api/current.py" "$current_dir" | awk 'NR==2')
 
-    # Return the randomly selected file path
-    echo "${files[$index]}"
+    if [ -n "$current_img" ]; then
+        echo "$current_img"
+        return 10
+    else
+        # Generate a random index within the range of available file paths
+        index=$((RANDOM % ${#files[@]}))
+        # Return the randomly selected file path
+        echo "${files[$index]}"
+        return "$sleep_time_default"
+    fi
 }
 
 # Function to get the average color of the img
@@ -44,9 +61,9 @@ get_hex() {
 
 # Function to set the background
 set_background() {
-    bg="$1" # argument 1 is the img path 
-    hex="$2" # argument 2 is the fill color as a hex value 
-    
+    bg="$1" # argument 1 is the img path
+    hex="$2" # argument 2 is the fill color as a hex value
+
     # Set the background using swww
     swww img "$bg" --no-resize --fill-color "$hex" -t grow --transition-pos 'top-right' --transition-duration 5
 }
@@ -57,18 +74,19 @@ swww-daemon & disown
 
 source "$scripts"/.venv/bin/activate
 
-# cd /home/david/Scripts/WallTune-Usage/imageaverage/ 
+# cd /home/david/Scripts/WallTune-Usage/imageaverage/
 
 while true; do
     # Get the next image
     image=$(get_file)
-    
+    sleep_time=$?
     echo "Changing to $image"
 
     hex=$(get_hex "$image")
 
     set_background "$image" "$hex"
 
-    sleep 60
+    echo "Waiting for $sleep_time"
+    sleep "$sleep_time"
 done
 
